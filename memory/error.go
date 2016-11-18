e0c137db8d7ee5fa5faf706dcf56a567e5f98848
Tim Schindler <xh3b4sd@gmail.com>
2016-11-19 00:32:06 +0100

    removed unused maskAnyf method

--- memory/error.go
+++ memory/error.go
@@ -1,26 +1,10 @@
 package memory
 
-import (
-	"fmt"
-
-	"github.com/juju/errgo"
-)
+import "github.com/juju/errgo"
 
 var (
 	maskAny = errgo.MaskFunc(errgo.Any)
 )
-
-func maskAnyf(err error, f string, v ...interface{}) error {
-	if err == nil {
-		return nil
-	}
-
-	f = fmt.Sprintf("%s: %s", err.Error(), f)
-	newErr := errgo.WithCausef(nil, errgo.Cause(err), f, v...)
-	newErr.(*errgo.Err).SetLocation(1)
-
-	return newErr
-}
 
 var noSuchFileOrDirectoryError = errgo.New("no such file or directory")
 
